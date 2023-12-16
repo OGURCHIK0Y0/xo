9f0537ef85db569e050216218fa99151ab40d562
OGURCHIK0Y0 <145265584+OGURCHIK0Y0@users.noreply.github.com>
2023-12-16 19:15:39 +0700

    Create sovp chisel.py

---
+++ sovp chisel.py
@@ -0,0 +1,11 @@
+def chisla(x,y): 
+    if x>9 and x<100 and y>9 and y<100:
+        if x == y:
+            print('совпадают')
+        else:
+            print('не совпадают')
+       
+    else:
+        print('не правильно')
+    
+chisla(100,100)

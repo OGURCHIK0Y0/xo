80cbfe531098f1fb9fccc314f878e4a89199f82f
OGURCHIK0Y0 <145265584+OGURCHIK0Y0@users.noreply.github.com>
2024-03-31 17:58:42 +0700

    Create ckjdfhb 3.py

---
+++ ckjdfhb 3.py
@@ -0,0 +1,25 @@
+users={}
+command= input('че хотите ')
+while command!="выход":
+    if command =='регистрация':
+        login = input("Введите логин ")
+        password1 = input("Введите пороль ")
+        password2=input("Введите пороль повторно ")
+        if password1 == password2:
+            print("вы зарегистрировались! ")
+            users[login]=password1
+        else:
+            print('регистрация не удалась ')
+            passwor2=input('повторите попытку ')
+    elif command == 'авторизация':
+        login = input('Введите логин ')
+        password = input('Введите ппороль ')
+        if login in users.keys():
+            if password == users[login]:
+                print('вы авторизовались! ')
+            else:
+                print("password не верный ")
+        else:
+            print('пользователь не найден ')
+    command = input('че хотие ')
+    
